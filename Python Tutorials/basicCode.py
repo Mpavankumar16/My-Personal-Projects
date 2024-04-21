58e5d26ec7884576d0790ceb4293647ac86c8c9e
pavankumar.munganda <pavankumar.munganda@servicenow.com>
2024-04-21 16:47:26 +0530

    Basics

---
+++ Python Tutorials/basicCode.py
@@ -0,0 +1,12 @@
+age =23
+x,y,z = 1,2,3
+x=y=z=5
+
+print("You are "+str(age)+" years old")
+print("You are", age,"years old")
+print(f"You are {age} years old")
+
+#Integer
+#Float
+#String
+#Boolean
